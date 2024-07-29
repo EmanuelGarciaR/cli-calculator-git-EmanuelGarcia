06fc6cb79caeb7a556bf6cb9ba136df39668b506
Ema <emanuelgarciarios@gmail.com>
2024-07-29 13:24:07 -0500

    Create add method

--- app/model.py
+++ app/model.py
@@ -1,16 +1,3 @@
 class Calculator:
-    def add(self, a: float, b: float) -> float:
-        return a + b
-    
-    def subtract(self, a: float, b: float) -> float:
-        return a - b
-    
-    def multiply(self, a: float, b: float) -> float:
-        return a * b
-    
-    def divide(self, a: float, b: float) -> float | str:
-        if b == 0:
-            return "Cannot divide by zero!"
-        return a / b
-    
-    
+    def add(self, num_1:float, num_2: float)-> float:
+        return num_1 + num_2
