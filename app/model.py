0a5ef60b05c4d476d9857187274481e8da4b688e
Ema <emanuelgarciarios@gmail.com>
2024-07-29 13:34:31 -0500

    Create susbtract, multiply, divide and divide methods

--- app/model.py
+++ app/model.py
@@ -1,3 +1,14 @@
 class Calculator:
     def add(self, num_1:float, num_2: float)-> float:
         return num_1 + num_2
+
+    def substract(self, num_1: float, num_2: float)-> float:
+        return num_1 - num_2
+
+    def multiply(self, num_1: float, num_2: float)-> float:
+        return num_1 * num_2
+
+    def divide(self, num_1: float, num_2: float)-> float | str:
+        if num_2 == 0:
+            return "Can not divide by zero!"
+        return num_1 / num_2
